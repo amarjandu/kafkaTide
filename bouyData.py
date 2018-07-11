3348a27efe5d91fe0ab1b77745a4406744e64388
amar jandu <amarjandu@gmail.com>
2018-07-10 19:25:52 -0700

    encapsulated bouyData object, created fetch and get data

---
+++ bouyData.py
@@ -0,0 +1,34 @@
+import time, urllib.request , json
+
+class bouyData:
+    noaaURL = 'https://tidesandcurrents.noaa.gov/api/datagetter?'
+    dataRequest = '&date=latest&format=json&time_zone=gmt&units=english' 
+    epoch_time = time.time()
+    url = None
+    data = {}
+    def __init__(self,id,fetch=False):
+        if id:
+            self.id = id.strip()
+            tempURL = self.noaaURL+'station='+ self.id + self.dataRequest
+            waterLevelTemp = tempURL + '&product=water_level&datum=MTL'
+            waterTemp =  tempURL + '&product=water_temperature' 
+            windTemp = tempURL + '&product=wind'
+            self.urlDict = {'waterLevelTemp':waterLevelTemp,'waterTemp':waterTemp,'windTemp':windTemp}
+            if fetch == True:
+                self.fetchUrl()
+        
+    def fetchUrl(self):
+        if len(self.urlDict) > 0:
+            for  url in self.urlDict:
+                if url != None:
+                    try:
+                        # dont know how to have
+                        self.data[url] = urllib.request.urlopen(self.urlDict[url]).read().decode("utf-8")
+                        self.error = None
+                    except urllib.error.URLError:
+                        self.requestData = None
+                        self.error = urllib.error.URLError
+
+    def getJson(self):
+        if len(self.data) > 0:
+            return {json.dumps({self.id:self.data})}
--- kakfa_producer.py
+++ kakfa_producer.py
@@ -1,12 +1,11 @@
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
-import urllib.request
+import urllib.request,bouyData
 
 
-producer = KafkaProducer(bootstrap_servers=['0.0.0.0:9092'])
+#producer = KafkaProducer(bootstrap_servers=['0.0.0.0:9092'])
 topic = 'tideData'
-noaaURL = 'https://tidesandcurrents.noaa.gov/api/datagetter?'
-dataRequest = '&date=latest&format=json&time_zone=gmt&units=english'
+
 cleanIDsFile = open('./config/cleanStationID','r')
 cleanIDs = []
 
@@ -14,14 +13,10 @@ for line in cleanIDsFile:
         cleanIDs.append(line)
 cleanIDsFile.close()
 
-def getData(ids,outArray):
-        tempURL = noaaURL+'station='+ ids.strip() + dataRequest
-        waterLevelTemp = tempURL + '&product=water_level&datum=MTL'
-        waterTemp = tempURL + '&product=water_temperature'
-        windTemp = tempURL + '&product=wind'
-        urlArray = [waterLevelTemp,waterTemp,windTemp]
-        for urls in urlArray:
-                requestData = urllib.request.urlopen(urls))
+for ids in cleanIDs:
+        temp = bouyData.bouyData(ids,False)
+        temp.fetchUrl()
+        print(temp.getJson())
 
 
     
